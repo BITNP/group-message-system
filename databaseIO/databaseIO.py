df42f3448e16369faabdc751267d5ddb95f235a0
FKYnJYQ <loveress01@gmail.com>
2018-07-01 01:44:18 +0800

    aftersendsingle finish

--- databaseIO/databaseIO.py
+++ databaseIO/databaseIO.py
@@ -39,59 +39,61 @@ class databaseIO:
         获得User表用户信息，默认id正确
             :param self: 
             :param id:int: 
-        """   
+        """
         self._connect()
 
         cur = self.conn.cursor()
         cur.execute(
-            'select fee,tplIDList,createTime,remark from User where id = %s limit 1',(id,))
+            'select fee,tplIDList,createTime,remark from User where id = %s limit 1', (id,))
         res = cur.fetchone()
         cur.close()
 
         self._close()
         return res
 
-    def getUserHighestUid(self,id):
+    def getUserHighestUid(self, id):
         self._connect()
 
         cur = self.conn.cursor()
         cur.execute(
-            'select uid from SendStat where id = %s order by uid DESC limit 1',(id,))
+            'select uid from SendStat where id = %s order by uid DESC limit 1', (id,))
         res = cur.fetchone()
-        res = 0 if res is None else res[0] 
+        res = 0 if res is None else res[0]
         cur.close()
 
         self._close()
         return res
 
-    def beforeSendSingle(self, id: int,mobile:str,text:str):
+    def beforeSendSingle(self, id: int, mobile: str, text: str):
 
-
-        next_uid = self.getUserHighestUid(id)+1 # 函数内对conn的操作有影响 TODO 
+        next_uid = self.getUserHighestUid(id)+1  # 函数内对conn的操作有影响 TODO
 
         self._connect()
 
         cur = self.conn.cursor()
-        
+
         res = cur.execute(
-            'Insert into SendStat(id,uid,mobile,content) values(%s,%s,%s,%s)',(id,next_uid,mobile,text))
+            'Insert into SendStat(id,uid,mobile,content) values(%s,%s,%s,%s)', (id, next_uid, mobile, text))
         self.conn.commit()
         cur.close()
 
         self._close()
-        return next_uid # 返回 uid
-    
-    def afterSendSingle(self,id,uid,fee,sid,code,msg):
+        return next_uid  # 返回 uid
+
+    def afterSendSingle(self, id, uid, fee, sid, code, msg):
         self._connect()
 
         cur = self.conn.cursor()
         res = cur.execute(
-            'update SendStat set fee=SendStat+%s,sid=%s,code=%s,msg=%s where id = %s and uid = %s',(id,uid))
+            'update SendStat set fee=SendStat.fee+%s,sid=%s,code=%s,msg=%s where id = %s and uid = %s', (
+                fee,sid,code,msg,id, uid)
+        )
+        res = cur.execute('update User set fee=User.fee+%s where id = %s',(fee,id))
         self.conn.commit()
         cur.close()
 
         self._close()
-        return res # 返回 uid
+        return res  # 返回 uid
 
     def InsertUser(self, username: str, password: str, remark: str):
         self._connect()
@@ -114,4 +116,5 @@ if __name__ == '__main__':
     # run.InsertUser('shetuan1','md6','bbb')
     # run.InsertUser('shetuan2','md7','ccc')
 
-    print(run.beforeSendSingle(1,'1123','hh'))
+    uid = (run.beforeSendSingle(1, '1123', 'hh'))
+    print(run.afterSendSingle(1,uid,0.02,1,2,'saf'))
